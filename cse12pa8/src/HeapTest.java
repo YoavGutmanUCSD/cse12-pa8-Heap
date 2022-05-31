d12d7324e2ce22275d7f0578f5c5464ce8b04fab
YoavGutmanUCSD <ygutman@ucsd.edu>
2022-05-31 12:46:17 -0700

    Revert "ayy"
    This reverts commit d5edf24f2e99105486936428e19aba7aac5fcf40.

--- cse12pa8/src/HeapTest.java
+++ cse12pa8/src/HeapTest.java
@@ -1,14 +1,6 @@
-import static org.junit.*;
-import static org.junit.Assert.*;
+// import static org.junit.*;
+
 import java.util.*;
 
-
 public class HeapTest {
- 
-    @Test
-    public void areYOUEmpty() {
-        assertEquals(0, 0);
-    }
-
-
 }
