d5edf24f2e99105486936428e19aba7aac5fcf40
Kathegnosis <g8keren@gmail.com>
2022-05-31 04:09:34 -0700

    ayy

--- cse12pa8/src/HeapTest.java
+++ cse12pa8/src/HeapTest.java
@@ -1,6 +1,14 @@
-// import static org.junit.*;
-
+import static org.junit.*;
+import static org.junit.Assert.*;
 import java.util.*;
 
+
 public class HeapTest {
+ 
+    @Test
+    public void areYOUEmpty() {
+        assertEquals(0, 0);
+    }
+
+
 }
